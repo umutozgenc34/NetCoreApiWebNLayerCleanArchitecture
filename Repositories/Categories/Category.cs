55763999c56a2dddb8eb3881c1f320facbada938
umutozgenc34 <umutozgenc34@hotmail.com>
2024-10-29 13:14:31 +0300

    Category init

---
+++ Repositories/Categories/Category.cs
@@ -0,0 +1,12 @@
+﻿
+
+using Repositories.Products;
+
+namespace Repositories.Categories;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Product>? Products { get; set; }
+}
--- Repositories/Products/Product.cs
+++ Repositories/Products/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace Repositories.Products;
+﻿using Repositories.Categories;
+
+namespace Repositories.Products;
 
 public class Product
 {
@@ -7,4 +9,6 @@ public class Product
     public string Name { get; set; } = default!;
     public decimal Price { get; set; }
     public int Stock { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; } = default!;
 }
